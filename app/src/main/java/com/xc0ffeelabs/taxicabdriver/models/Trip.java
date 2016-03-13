f633ae16112435ea260564cd86ff99ef377e8970
Srikant Kammila <srikanth1234@gmail.com>
2016-03-12 20:40:29 -0800

    push notifications

---
+++ app/src/main/java/com/xc0ffeelabs/taxicabdriver/models/Trip.java
@@ -0,0 +1,42 @@
+package com.xc0ffeelabs.taxicabdriver.models;
+
+import com.parse.ParseClassName;
+import com.parse.ParseUser;
+
+/**
+ * Created by skammila on 3/12/16.
+ */
+@ParseClassName("Trip")
+public class Trip {
+    private ParseUser user;
+    private Driver driver;
+    private String tripStatus;
+
+    public Trip() {
+
+    }
+
+    public ParseUser getUser() {
+        return user;
+    }
+
+    public void setUser(ParseUser user) {
+        this.user = user;
+    }
+
+    public String getTripStatus() {
+        return tripStatus;
+    }
+
+    public void setTripStatus(String tripStatus) {
+        this.tripStatus = tripStatus;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+}
---
+++ app/src/main/java/com/xc0ffeelabs/taxicabdriver/services/TripManager.java
@@ -0,0 +1,7 @@
+package com.xc0ffeelabs.taxicabdriver.services;
+
+/**
+ * Created by skammila on 3/12/16.
+ */
+public class TripManager {
+}
