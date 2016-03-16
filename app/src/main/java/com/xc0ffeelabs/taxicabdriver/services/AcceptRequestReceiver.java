76b7d477bafe2512673112fa8db64baa06689466
Srikant Kammila <srikanth1234@gmail.com>
2016-03-15 18:14:37 -0700

    driver notification changes

---
+++ app/src/main/java/com/xc0ffeelabs/taxicabdriver/services/AcceptRequestReceiver.java
@@ -0,0 +1,74 @@
+package com.xc0ffeelabs.taxicabdriver.services;
+
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.parse.FunctionCallback;
+import com.parse.ParseCloud;
+import com.parse.ParseException;
+import com.xc0ffeelabs.taxicabdriver.activities.MapActivity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by skammila on 3/14/16.
+ */
+public class AcceptRequestReceiver extends BroadcastReceiver {
+    private static final String ACCEPT_ACTION = "com.xc0ffeelabs.taxicabdriver.ACCEPT_REQUEST";
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d("Debug", "In Accept handler");
+        String action = intent.getAction();
+        Bundle bnd = intent.getExtras();
+        String tripId = bnd.getString("tripId");
+        String driverId = bnd.getString("driverId");
+        NotificationManager mNotificationManager = (NotificationManager) context
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.cancel(DriverNotificationReceiver.NOTIFICATION_ID);
+
+        if(ACCEPT_ACTION.equals(action)) {
+            try {
+                Log.d("Debug", "TripId " + tripId);
+                Map parameters = new HashMap();
+                parameters.put("driverId", driverId);
+                parameters.put("tripId", tripId);
+
+                ParseCloud.callFunctionInBackground("driverAcceptTrip", parameters, new FunctionCallback() {
+                    @Override
+                    public void done(Object object, ParseException e) {
+                        if (e == null) {
+                            Log.i("Success", "Driver assigned to the trip");
+                        } else {
+                            Log.e("Error", "Driver not assigned to the trip");
+                        }
+                    }
+
+                    @Override
+                    public void done(Object o, Throwable throwable) {
+                        if (throwable == null) {
+                            Log.i("Success", "Driver assigned to the trip");
+                        } else {
+                            Log.e("Error", "Driver not assigned to the trip");
+                        }
+                    }
+                });
+//                ParseQuery tripQ = ParseQuery.getQuery("Trip");
+//                ParseObject trip = tripQ.get(tripId);
+//                trip.put("status", "confirmed");
+//                trip.put("state", "driver-accepted-trip-request");
+//                trip.saveInBackground();
+
+                Intent intentMap = new Intent(context, MapActivity.class);
+                intentMap.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(intentMap);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/xc0ffeelabs/taxicabdriver/services/DriverNotificationReceiver.java
@@ -0,0 +1,146 @@
+package com.xc0ffeelabs.taxicabdriver.services;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.xc0ffeelabs.taxicabdriver.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Iterator;
+
+/**
+ * Created by skammila on 3/13/16.
+ */
+public class DriverNotificationReceiver  extends BroadcastReceiver {
+    private static final String TAG = "NotificationReceiver";
+    public static final String intentAction = "com.parse.push.intent.RECEIVE";
+
+    private static final String ACCEPT_ACTION = "com.xc0ffeelabs.taxicabdriver.ACCEPT_REQUEST";
+    private static final String DENY_ACTION = "com.xc0ffeelabs.taxicabdriver.DENY_REQUEST";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent == null) {
+            Log.d(TAG, "Receiver intent null");
+        } else {
+            // Parse push message and handle accordingly
+            processPush(context, intent);
+        }
+
+    }
+
+    private void processPush(Context context, Intent intent) {
+        String action = intent.getAction();
+        Log.d(TAG, "got action " + action);
+        if (action.equals(intentAction)) {
+            String channel = intent.getExtras().getString("com.parse.Channel");
+            try {
+                JSONObject json = new JSONObject(intent.getExtras().getString("com.parse.Data"));
+                Log.d(TAG, "got action " + action + " on channel " + channel + " with:");
+                // Iterate the parse keys if needed
+                Iterator<String> itr = json.keys();
+                while (itr.hasNext()) {
+                    String key = (String) itr.next();
+                    String value = json.getString(key);
+                    JSONObject valJson = new JSONObject(value);
+                    Log.d(TAG, "..." + key + " => " + value);
+                    // Extract custom push data
+                    if (key.equals("customdata")) {
+                        // create a local notification
+                        createNotification(context, valJson);
+                    } else if (key.equals("launch")) {
+                        // Handle push notification by invoking activity directly
+//                        launchSomeActivity(context, value);
+                    } else if (key.equals("broadcast")) {
+                        // OR trigger a broadcast to activity
+//                        triggerBroadcastToActivity(context, value);
+                    }
+                }
+            } catch (JSONException ex) {
+                Log.d(TAG, "JSON failed!");
+            }
+        }
+
+
+    }
+
+    public static final int NOTIFICATION_ID = 45;
+
+    // Create a local dashboard notification to tell user about the event
+    // See: http://guides.codepath.com/android/Notifications
+    private void createNotification(Context context, JSONObject datavalue) throws JSONException {
+
+
+//        ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+//        List<ActivityManager.RunningTaskInfo> taskInfo = am.getRunningTasks(1);
+//        ComponentName componentInfo = taskInfo.get(0).topActivity;
+//        if(componentInfo.getPackageName().equalsIgnoreCase("com.xc0ffeelabs.taxicabdriver")){
+//            //Activity Running
+////            Send a broadcast with the intent-filter which you register in your activity
+////            where you want to have the updates
+
+//        Intent pupInt = new Intent(context, MapActivity.class);
+//        pupInt.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//        pupInt.putExtra("data", datavalue);
+//        context.getApplicationContext().startActivity(pupInt);
+//
+//
+//        }
+//        else {
+
+
+        //deny intent
+        Intent requestDenyInt = new Intent(DENY_ACTION);
+        requestDenyInt.putExtra("tripId", datavalue.getString("tripId"));
+        requestDenyInt.putExtra("driverId", datavalue.getString("driverId"));
+        PendingIntent pendingIntentDeny = PendingIntent.getBroadcast(context, 12345, requestDenyInt, Intent.FILL_IN_DATA);
+
+        //accept intent
+        Intent requestAcceptInt = new Intent(ACCEPT_ACTION);
+        requestAcceptInt.putExtra("tripId", datavalue.getString("tripId"));
+        requestAcceptInt.putExtra("driverId", datavalue.getString("driverId"));
+        PendingIntent pendingIntentAccept = PendingIntent.getBroadcast(context, 12345, requestAcceptInt, Intent.FILL_IN_DATA);
+
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.drawable.common_google_signin_btn_icon_light)
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText(datavalue.getString("text")))
+                .setContentTitle("Request")
+                .setContentText("Request for Pickup")
+                .setPriority(Notification.PRIORITY_MAX)
+                .addAction(R.drawable.thumbs_up, "Accept", pendingIntentAccept)
+                .addAction(R.drawable.thumb_down_sm, "Deny", pendingIntentDeny);
+        NotificationManager mNotificationManager = (NotificationManager) context
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+//        }
+    }
+
+//    // Handle push notification by invoking activity directly
+//    // See: http://guides.codepath.com/android/Using-Intents-to-Create-Flows
+//    private void launchSomeActivity(Context context, String datavalue) {
+//        Intent pupInt = new Intent(context, MapActivity.class);
+//        pupInt.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//        pupInt.putExtra("data", datavalue);
+//        context.getApplicationContext().startActivity(pupInt);
+//    }
+//
+//    // Handle push notification by sending a local broadcast
+//    // to which the activity subscribes to
+//    // See: http://guides.codepath.com/android/Starting-Background-Services#communicating-with-a-broadcastreceiver
+//    private void triggerBroadcastToActivity(Context context, String datavalue) {
+//        Intent intent = new Intent(intentAction);
+//        intent.putExtra("data", datavalue);
+//        LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
+//    }
+
+}
