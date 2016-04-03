ccfcd3bdd7b943da2b377638948375407c022456
Srikant Kammila <srikanth1234@gmail.com>
2016-04-02 21:43:58 -0700

    notification add  image, format text

---
+++ app/src/main/java/com/xc0ffeelabs/taxicabdriver/services/CreateNotification.java
@@ -0,0 +1,171 @@
+package com.xc0ffeelabs.taxicabdriver.services;
+
+import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.xc0ffeelabs.taxicabdriver.R;
+import com.xc0ffeelabs.taxicabdriver.models.User;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URL;
+
+/**
+ * Created by skammila on 4/2/16.
+ */
+public class CreateNotification extends IntentService {
+    private String text;
+    private String userId;
+    private String driverId;
+    private String tripId;
+    public CreateNotification() {
+        super("CreateNotification");
+    }
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        String dataValue = intent.getStringExtra("dataValue");
+        JSONObject dataJson = null;
+        try {
+            dataJson = new JSONObject(dataValue);
+            userId = dataJson.getString("userId");
+            driverId = dataJson.getString("driverId");
+            tripId = dataJson.getString("tripId");
+        } catch (JSONException e) {
+            e.printStackTrace();
+//            stopSelf();
+            return;
+        }
+        ParseUser.getQuery().include("pickUpLocation").include("destLocation").getInBackground(userId, new GetCallback<ParseUser>() {
+            @Override
+            public void done(ParseUser object, ParseException e) {
+                User user = (User) object;
+                String userProfileImage = null;
+                if (user != null) {
+                    try {
+                        text = "";
+                        userProfileImage = user.getProfileImage();
+//                        if (userProfileImage == null || userProfileImage.length() == 0) {
+//                            userProfileImage = "http://salescafe.us/sites/default/files/styles/50x50/public/pictures/M3%20avatar.png";
+//                        }
+//                        URL url = new URL(userProfileImage);
+//                        image = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+                        String name = user.getName();
+                        text += name + " needs a ride. ";
+                        user.getPickupLocation().fetchIfNeeded();
+                        String pickupLocation = user.getPickupLocation().getText();
+                        if (pickupLocation != null && pickupLocation.length() > 0) {
+                            text += "\nPICKUP: " + pickupLocation + " ";
+                        }
+                        user.getDestLocation().fetchIfNeeded();
+                        String destLocation = user.getDestLocation().getText();
+                        if (destLocation != null && destLocation.length() > 0) {
+                            text += "\nDESTINATION: " + destLocation + " ";
+                        }
+                        text += "\nCan you pick " + name + "?";
+                        Log.i("CreateNotification", name + "  pickup " + pickupLocation + " dest " + destLocation);
+                    } catch (ParseException e1) {
+                        e1.printStackTrace();
+                    }
+                } else {
+                    text = "User needs a ride. Can you pick the user?";
+                }
+
+                dowloadProfileImage(userProfileImage);
+            }
+        });
+
+
+
+    }
+
+    private void dowloadProfileImage(final String profileImage) {
+        final Handler imageHandler = new Handler(){
+            public void handleMessage(Message msg) {
+                if(msg.obj!=null && msg.obj instanceof Bitmap){
+                    showNotification(text, userId, driverId, tripId, (Bitmap)msg.obj);
+                }
+
+            };
+        };
+
+        new Thread(){
+            public void run() {
+                try {
+                    Bitmap image;
+                    if (profileImage != null && profileImage.length() > 0) {
+                        URL url = new URL(profileImage);
+                        image = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+                    } else {
+                        image = BitmapFactory.decodeResource(getApplicationContext().getResources(),
+                                R.drawable.profile_avatar);
+                    }
+
+//                    Bitmap bitmap = BitmapFactory.decodeStream((InputStream)new URL(profileImage);
+                    Message msg = new Message();
+                    msg.obj = image;
+                    imageHandler.sendMessage(msg);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
+    protected void showNotification(String msg, String userId, String driverId, String tripId, Bitmap image) {
+        //create a notification
+
+        //deny intent
+        Intent requestDenyInt = new Intent(RideRequestActionReceiver.DENY_ACTION);
+        requestDenyInt.putExtra("tripId", tripId);
+        requestDenyInt.putExtra("driverId", driverId);
+        requestDenyInt.putExtra("userId", userId);
+        int rnd = (int)System.currentTimeMillis();
+        PendingIntent pendingIntentDeny = PendingIntent.getBroadcast(getApplicationContext(), rnd, requestDenyInt, Intent.FILL_IN_DATA);
+
+        //accept intent
+        Intent requestAcceptInt = new Intent(RideRequestActionReceiver.ACCEPT_ACTION);
+        requestAcceptInt.putExtra("tripId", tripId);
+        requestAcceptInt.putExtra("driverId", driverId);
+        requestAcceptInt.putExtra("userId", userId);
+        PendingIntent pendingIntentAccept = PendingIntent.getBroadcast(getApplicationContext(), rnd, requestAcceptInt, Intent.FILL_IN_DATA);
+
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext())
+                .setSmallIcon(R.drawable.ic_chariot_logo_9)
+                .setLargeIcon(image)
+                .setColor(getResources().getColor(R.color.colorPrimary))
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
+                .setContentTitle("Ride request")
+                .setContentText("Request for ride")
+                .setPriority(Notification.PRIORITY_MAX)
+                .addAction(R.drawable.thumbs_up, "Accept", pendingIntentAccept)
+                .addAction(R.drawable.thumb_down_sm, "Deny", pendingIntentDeny);
+
+        NotificationManager mNotificationManager = (NotificationManager) getApplicationContext()
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+
+        Notification note = mBuilder.build();
+        note.flags |= Notification.FLAG_AUTO_CANCEL; // For Non cancellable notification
+        mNotificationManager.notify(DriverNotificationReceiver.NOTIFICATION_ID, note);
+
+        //set auto remove notification alarm
+        RideRequestActionReceiver.setAutocancelAlarm(getApplicationContext());
+        Log.i("Default Notification", "Default Notification");
+        stopSelf();
+    }
+
+}
--- app/src/main/java/com/xc0ffeelabs/taxicabdriver/services/DriverNotificationReceiver.java
+++ app/src/main/java/com/xc0ffeelabs/taxicabdriver/services/DriverNotificationReceiver.java
@@ -1,18 +1,9 @@
 package com.xc0ffeelabs.taxicabdriver.services;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.app.NotificationCompat;
 import android.util.Log;
-
-import com.parse.ParseException;
-import com.parse.ParseUser;
-import com.xc0ffeelabs.taxicabdriver.R;
-import com.xc0ffeelabs.taxicabdriver.models.User;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -60,7 +51,7 @@ public class DriverNotificationReceiver  extends BroadcastReceiver {
                     // Extract custom push data
                     if (key.equals("customdata")) {
                         // create a local notification
-                        createNotification(context, valJson);
+                        createNotification(context, value);
                     } else if (key.equals("launch")) {
                         // Handle push notification by invoking activity directly
 //                        launchSomeActivity(context, value);
@@ -81,7 +72,7 @@ public class DriverNotificationReceiver  extends BroadcastReceiver {
 
     // Create a local dashboard notification to tell user about the event
     // See: http://guides.codepath.com/android/Notifications
-    private void createNotification(Context context, JSONObject datavalue) throws JSONException {
+    private void createNotification(Context context, String datavalue) throws JSONException {
 
 
 //        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
@@ -100,67 +91,37 @@ public class DriverNotificationReceiver  extends BroadcastReceiver {
 //        }
 
         //destination
-        User user = null;
-        String locationAddress = null;
-        String userName = null;
-        String message = "User requesting for ride. Can you take this ride?";
-        try {
-            user = (User)ParseUser.getQuery().include("destLocation").get(datavalue.getString("userId"));
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        if (user != null) {
-//            try {
-//                user.getDestLocation().fetchIfNeeded();
-//            } catch (ParseException e) {
-//                e.printStackTrace();
-//            }
-            locationAddress = user.getDestLocation().getText();
-            userName = user.getName();
-        }
+//        User user = null;
+//        String locationAddress = null;
+//        String userName = null;
+//        String message = "User requesting for ride. Can you take this ride?";
+//        try {
+//            user = (User)ParseUser.getQuery().include("destLocation").get(datavalue.getString("userId"));
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+//        if (user != null) {
+////            try {
+////                user.getDestLocation().fetchIfNeeded();
+////            } catch (ParseException e) {
+////                e.printStackTrace();
+////            }
+//            locationAddress = user.getDestLocation().getText();
+//            userName = user.getName();
+//        }
+//
+//        if (locationAddress != null && userName != null) {
+//            message = userName + " is requesting for a ride to " + locationAddress + ". Can you pick " + locationAddress + "?";
+//        }
 
-        if (locationAddress != null && userName != null) {
-            message = userName + " is requesting for a ride to " + locationAddress + ". Can you pick " + locationAddress + "?";
-        }
-
-
-        //create a notification
-
-        //deny intent
-        Intent requestDenyInt = new Intent(RideRequestActionReceiver.DENY_ACTION);
-        requestDenyInt.putExtra("tripId", datavalue.getString("tripId"));
-        requestDenyInt.putExtra("driverId", datavalue.getString("driverId"));
-        requestDenyInt.putExtra("userId", datavalue.getString("userId"));
-        int rnd = (int)System.currentTimeMillis();
-        PendingIntent pendingIntentDeny = PendingIntent.getBroadcast(context, rnd, requestDenyInt, Intent.FILL_IN_DATA);
-
-        //accept intent
-        Intent requestAcceptInt = new Intent(RideRequestActionReceiver.ACCEPT_ACTION);
-        requestAcceptInt.putExtra("tripId", datavalue.getString("tripId"));
-        requestAcceptInt.putExtra("driverId", datavalue.getString("driverId"));
-        requestAcceptInt.putExtra("userId", datavalue.getString("userId"));
-        PendingIntent pendingIntentAccept = PendingIntent.getBroadcast(context, rnd, requestAcceptInt, Intent.FILL_IN_DATA);
+//        CreateNotification notif = new CreateNotification();
+        Intent it = new Intent(context, CreateNotification.class);
+//        it.putExtra("userId", datavalue.getString("userId"));
+        it.putExtra("dataValue", datavalue);
+        context.startService(it);
 
 
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
-                .setSmallIcon(R.drawable.ic_chariot_logo_9)
-                .setStyle(new NotificationCompat.BigTextStyle()
-                        .bigText(message))
-                .setContentTitle("Ride request")
-                .setContentText("Request for ride")
-                .setPriority(Notification.PRIORITY_MAX)
-                .addAction(R.drawable.thumbs_up, "Accept", pendingIntentAccept)
-                .addAction(R.drawable.thumb_down_sm, "Deny", pendingIntentDeny);
 
-        NotificationManager mNotificationManager = (NotificationManager) context
-                .getSystemService(Context.NOTIFICATION_SERVICE);
-
-        Notification note = mBuilder.build();
-        note.flags |= Notification.FLAG_AUTO_CANCEL; // For Non cancellable notification
-        mNotificationManager.notify(NOTIFICATION_ID, note);
-
-        //set auto remove notification alarm
-        RideRequestActionReceiver.setAutocancelAlarm(context);
 
 //        }
     }
